601c1a97c7022bc65870bfd7d9d4900499382b7f
SPiedra955 <spiedramite@cifpfbmoll.eu>
2023-02-14 23:44:10 +0100

    ultimo historia de usuario completada

--- src/main/java/edu/craptocraft/ricksy/bussiness/RickMenuDispatcher.java
+++ src/main/java/edu/craptocraft/ricksy/bussiness/RickMenuDispatcher.java
@@ -1,31 +1,32 @@
 package edu.craptocraft.ricksy.bussiness;
 
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class RickMenuDispatcher implements GuestDispatcher{
 
     private int stockMenu = 100;
     private double priceMenu = 10d;
+    private List<String> menu = new ArrayList<String>();
 
     RickMenuDispatcher(){};
 
     @Override
     public void dispatch(CreditCard card){
 
-        if(stockMenu > 0 && card.pay(priceMenu)) {
+        if(this.stockMenu > 0 && card.credit() >= priceMenu) {
             card.pay(priceMenu);
             stockMenu -= 1;
+            this.menu.add(card.cardOwner());
         }
     }
 
     int stockMenu(){
         return this.stockMenu;
     }
-
     @Override
     public String toString(){
-        return("Menús disponibles: " + stockMenu + "\n" +
-                "coste del menú : " + priceMenu);
+        return("Menús disponibles: " + stockMenu() + "\n" + menu);
     }
 
 
